1e1b44438cebd2b39713e0cc68d4f18457e047fb
sonali-sahu-05 <156996475+sonali-sahu-05@users.noreply.github.com>
2024-09-27 15:31:34 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,26 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
+function romanToInt(s) {
+    let romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
-};
-
-
-module.exports={romanToInt}
+    let total = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        let current = romanMap[s[i]];
+        let next = romanMap[s[i + 1]];
+        
+        if (next && current < next) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+    
+    return total;
+}
